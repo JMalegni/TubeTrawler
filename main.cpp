e1bd5c4cc07e53a6b140e74b411904c71d4aa88a
JMalegni <jwmalegni@gmail.com>
2023-11-20 15:57:59 -0500

    added a c++ source file that will open terminal and execute the python file, next update will be to implement SFML to make a user inteface

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+
+    // need to have python as path variable
+    //42nrFpATLow is a youtube video id that is temporary, I will make it so that we can get it as input from the user
+    system("cd .. && python main.py 42nrFpATLow");
+
+
+    return 0;
+
+}
