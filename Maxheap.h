521ec78a955a56e31bc9dcfea25befdadb1c3fec
jasonlin15 <jasonlin1@ufl.edu>
2023-12-04 15:50:30 -0500

    Have a nice day, fixed comments

--- Maxheap.h
+++ Maxheap.h
@@ -43,7 +43,9 @@ public:
         long long int right = 0;
         long long int parent = 0;
         long long int max = 0;
-        while (left < heap.size() and right < heap.size()) {
+        //check to see if the left and right indexes are still valid
+        while (left < heap.size() and right < heap.size()){
+            //set the parent index
             parent = max;
             left = parent * 2 + 1;
             right = parent * 2 + 2;
@@ -53,6 +55,7 @@ public:
             if (heap[left].likes > heap[max].likes) {
                 max = left;
             }
+            //swap the max index with the parent, could be left, right, or no changce
             std::swap(heap[max], heap[parent]);
             if (max == parent) {
                 break;
@@ -65,12 +68,15 @@ public:
         result.push_back(heap[0].word);
         result.push_back(std::to_string(heap[0].likes));
         result.push_back(std::to_string(heap[0].replies));
+        //make the last item become first one
         heap[0] = heap[heap.size() - 1];
+        //remove the last item
         heap.pop_back();
         heapify_down();
         return result;
     }
     void insert(std::string word, int likes, int replies) {
+        //capture a Heap_data with temp
         Heap_data temp;
         temp.word = word;
         temp.likes = likes;
--- RBT.h
+++ RBT.h
@@ -8,9 +8,10 @@
 
 using namespace std;
 
-class RBT {
+class RBT { //RedBlack Tree structure with left and right node attributes and variables for comment, likes, and replies
 private:
     struct Node {
+        //default value initialization
         string comment = "";
         string color = "red";
         int replies = 0;
@@ -19,15 +20,15 @@ private:
         Node* left;
         Node* right;
         Node* parent = nullptr;
-        Node(string x, int y, int z) : comment(x), replies(y), likes(z), left(nullptr), right(nullptr) {}
+        Node(string x, int y, int z) : comment(x), replies(y), likes(z), left(nullptr), right(nullptr) {} //constructor
     };
 
-    Node* root = nullptr;
-    void fixViolations(Node* node);
+    Node* root = nullptr; //root node instantiation
+    void fixViolations(Node* node); //private function declarations
     void leftRotation(Node* node);
     void rightRotation(Node* node);
 public:
-    void insert(string comment, int replies, int likes);
+    void insert(string comment, int replies, int likes); //public function declarations
     RBT::Node* getRoot();
     vector<string> mostLiked(Node* root);
     void readFile(RBT& myRBT);
@@ -35,22 +36,22 @@ public:
 
 void RBT::insert(string comment, int replies, int likes)
 {
-    Node* newNode = new Node(comment, replies, likes);
+    Node* newNode = new Node(comment, replies, likes); //defining node values for insert
     Node* parent = nullptr;
     Node* current = root;
     while (current != nullptr)
     {
         parent = current;
-        if (newNode->likes < current->likes)
+        if (newNode->likes < current->likes) //comment like is less than previous node, go left
         {
             current = current->left;
         }
-        else if (newNode->likes > current->likes)
+        else if (newNode->likes > current->likes) //comment like is greater than previous node, go right
         {
             current = current->right;
         }
         else {
-            current = current->right; // comment like amount is equal, hardcoded to go right for now
+            current = current->right; //comment like amount is equal, go right
         }
     }
     newNode->parent = parent;
@@ -58,19 +59,19 @@ void RBT::insert(string comment, int replies, int likes)
     {
         root = newNode; // This is the first node added to the tree
     }
-    else if (newNode->likes < parent->likes)
+    else if (newNode->likes < parent->likes) //comment like less, go left
     {
         parent->left = newNode;
     }
-    else if (newNode->likes > parent->likes)
+    else if (newNode->likes > parent->likes) //comment like greater, go right
     {
         parent->right = newNode;
     }
     else
     {
-        parent->right = newNode; // comment like amount is equal, hardcoded to go right for now
+        parent->right = newNode; //comment like amount is equal, hardcoded to go right
     }
-    fixViolations(newNode);
+    fixViolations(newNode); //call function to fix RedBlack tree violations in the tree
 }
 
 void RBT::fixViolations(Node* node)
@@ -79,44 +80,44 @@ void RBT::fixViolations(Node* node)
     {
         if ((node->parent == node->parent->parent->left))
         {
-            Node* uncle = node->parent->parent->right;
+            Node* uncle = node->parent->parent->right; //assign node to preform operation
             if ((uncle != nullptr) and (uncle->color == "red"))
             {
-                node->parent->color = "black";
+                node->parent->color = "black"; //set colors of nodes and assign node swaps
                 uncle->color = "black";
-                node->parent->parent->color = "red"; //may not be needed
+                node->parent->parent->color = "red";
                 node = node->parent->parent;
             }
             else
             {
-                if (node == node->parent->right)
+                if (node == node->parent->right) //other case, preform left rotation on parent
                 {
                     node = node->parent;
                     leftRotation(node);
                 }
-                node->parent->color = "black";
+                node->parent->color = "black"; //assign colors and right rotate grandparent
                 node->parent->parent->color = "red";
                 rightRotation(node->parent->parent);
             }
         }
         else
         {
-            Node* uncle = node->parent->parent->left;
+            Node* uncle = node->parent->parent->left; //assign node to preform operation
             if ((uncle != nullptr) and (uncle->color == "red"))
             {
-                node->parent->color = "black";
+                node->parent->color = "black"; //set colors of nodes and assign node swaps
                 uncle->color = "black";
-                node->parent->parent->color = "red"; //may not be needed
+                node->parent->parent->color = "red";
                 node = node->parent->parent;
             }
             else
             {
-                if (node == node->parent->left)
+                if (node == node->parent->left) //other case, preform right rotation on parent
                 {
                     node = node->parent;
                     rightRotation(node);
                 }
-                node->parent->color = "black";
+                node->parent->color = "black"; //assign colors and left rotate grandparent
                 node->parent->parent->color = "red";
                 leftRotation(node->parent->parent);
             }
@@ -127,9 +128,9 @@ void RBT::fixViolations(Node* node)
 
 void RBT::leftRotation(Node* node)
 {
-    Node* rChild = node->right;
+    Node* rChild = node->right; //assign nodes
     node->right = rChild->left;
-    if (rChild->left != nullptr)
+    if (rChild->left != nullptr) //check nodes exist
     {
         rChild->left->parent = node;
     }
@@ -138,7 +139,7 @@ void RBT::leftRotation(Node* node)
     {
         root = rChild;
     }
-    else if (node == node->parent->left)
+    else if (node == node->parent->left) //assign node children respectively
     {
         node->parent->left = rChild;
     }
@@ -152,9 +153,9 @@ void RBT::leftRotation(Node* node)
 
 void RBT::rightRotation(Node* node)
 {
-    Node* lChild = node->left;
+    Node* lChild = node->left; //assign nodes
     node->left = lChild->right;
-    if (lChild->right != nullptr)
+    if (lChild->right != nullptr) //check nodes exist
     {
         lChild->right->parent = node;
     }
@@ -163,7 +164,7 @@ void RBT::rightRotation(Node* node)
     {
         root = lChild;
     }
-    else if (node == node->parent->left)
+    else if (node == node->parent->left) //assign node children respectively
     {
         node->parent->left = lChild;
     }
@@ -175,22 +176,21 @@ void RBT::rightRotation(Node* node)
     node->parent = lChild;
 }
 
-RBT::Node* RBT::getRoot()
+RBT::Node* RBT::getRoot() //return root for most_liked function
 {
     return root;
 }
 
-vector<string> RBT::mostLiked(Node* root)
+vector<string> RBT::mostLiked(Node* root) //return right most node of tree which has most likes
 {
-    vector<string> comment_attributes;
+    vector<string> comment_attributes; //declare vector to hold comment attributes
     Node* current = root;
     while (current->right != nullptr)
     {
         current = current->right;
     }
-    comment_attributes.push_back(current->comment);
+    comment_attributes.push_back(current->comment); //pushback values to vector
     comment_attributes.push_back(to_string(current->likes));
     comment_attributes.push_back(to_string(current->replies));
-    return comment_attributes;
-    //cout << "The most liked comment has " << current->likes << " likes." << endl << "The comment is: " << current->comment << endl;
+    return comment_attributes; //return vector for display in GUI
 }
